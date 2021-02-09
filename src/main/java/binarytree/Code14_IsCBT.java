1ddef4da7174160b593a9a266e99323ec55d200d
diaoyuhang <951900584@qq.com>
2021-02-09 20:50:18 +0800

    返回给定二叉树是不是完全二叉树

--- src/main/java/binarytree/Code14_IsCBT.java
+++ src/main/java/binarytree/Code14_IsCBT.java
@@ -8,8 +8,33 @@ import java.util.LinkedList;
  * @Date: 21:05 2021/2/8
  */
 public class Code14_IsCBT {
-    public static void main(String[] args) {
+    // for test
+    public static Node generateRandomBST(int maxLevel, int maxValue) {
+        return generate(1, maxLevel, maxValue);
+    }
 
+    // for test
+    public static Node generate(int level, int maxLevel, int maxValue) {
+        if (level > maxLevel || Math.random() < 0.5) {
+            return null;
+        }
+        Node head = new Node((int) (Math.random() * maxValue));
+        head.left = generate(level + 1, maxLevel, maxValue);
+        head.right = generate(level + 1, maxLevel, maxValue);
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int maxLevel = 5;
+        int maxValue = 100;
+        int testTimes = 1000000;
+        for (int i = 0; i < testTimes; i++) {
+            Node head = generateRandomBST(maxLevel, maxValue);
+            if (isCBT1(head) != isCBT2(head)) {
+                System.out.println("Oops!");
+            }
+        }
+        System.out.println("finish!");
     }
 
     public static class Node {
@@ -47,8 +72,8 @@ public class Code14_IsCBT {
                 queue.add(r);
             }
 
-            if (l == null && r == null) {
-                leaf = true; //如果左右的节点都为null，就表示当前节点及以后的节点都应是叶子节点
+            if (l == null || r == null) {
+                leaf = true; //如果左右的节点有一个为null，就表示当前节点及以后的节点都应是叶子节点
             }
         }
         return true;
