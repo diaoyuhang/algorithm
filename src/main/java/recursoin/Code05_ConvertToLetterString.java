3c61c3d07ddc580faaa3a3662ec7089ba8710424
diaoyuhang <951900584@qq.com>
2021-02-16 21:28:32 +0800

    从左往右的尝试模型，转换数字字符串为字母字符串

--- src/main/java/recursoin/Code05_ConvertToLetterString.java
+++ src/main/java/recursoin/Code05_ConvertToLetterString.java
@@ -22,23 +22,58 @@ public class Code05_ConvertToLetterString {
         if (chars[i] == '0') {
             return 0;
         }
-        int sum = 0;
+
         if (chars[i] == '2') {
-            if (i + 1 < chars.length && Integer.valueOf(chars[i + 1]) <= 6) {
+            int sum = process(chars, i + 1);
+            if (i + 1 < chars.length && Integer.valueOf(chars[i + 1] + "") <= 6) {
                 sum += process(chars, i + 2);
             }
+            return sum;
         }
         if (chars[i] == '1') {
+            int sum = process(chars, i + 1);
             if (i + 1 < chars.length) {
                 sum += process(chars, i + 2);
             }
+            return sum;
         }
-        sum += process(chars, i + 1);
-        return sum;
+        return process(chars, i + 1);
     }
 
+    public static int dpWays(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        char[] chars = str.toCharArray();
+        int length = chars.length;
+        int[] dp = new int[length + 1];
+        dp[length] = 1;
+
+        for (int i = length - 1; i >= 0; i--) {
+            if (chars[i] == '0') {
+                dp[i] = 0;
+            }
+
+            if (chars[i] == '2') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < chars.length && Integer.valueOf(chars[i + 1] + "") <= 6) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+            if (chars[i] == '1') {
+                dp[i] = dp[i + 1];
+                if (i + 1 < chars.length) {
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(number("1023456789"));
+        System.out.println(number("121"));
+        System.out.println(dpWays("121"));
     }
 
 }
