9830802d3af1b5a9d9a2f45ddab0345fa0ef3a7c
diaoyuhang <951900584@qq.com>
2021-02-21 21:04:13 +0800

    贴纸数

--- src/main/java/dynamicprogram/Code02_StickersToSpellWord.java
+++ src/main/java/dynamicprogram/Code02_StickersToSpellWord.java
@@ -1,5 +1,7 @@
 package dynamicprogram;
 
+import java.util.HashMap;
+
 /**
  * 给定一个字符串str，给定一个字符串类型的数组arr。
  * arr里的每一个字符串，代表一张贴纸，你可以把单个字符剪开使用，目的是拼出str来。
@@ -12,7 +14,9 @@ package dynamicprogram;
  */
 public class Code02_StickersToSpellWord {
     public static void main(String[] args) {
-
+        String[] stickers = {"ababac", "cdcccd", "bdbac"};
+        String target = "ababa";
+        System.out.println(minStickers1(stickers, target));
     }
 
     public static int minStickers1(String[] stickers, String target) {
@@ -27,7 +31,48 @@ public class Code02_StickersToSpellWord {
                 map[i][c - 'a']++;
             }
         }
-
-        return 0;
+        HashMap<String, Integer> dp = new HashMap<>();
+        dp.put("", 0);
+        return process(dp, map, target);
     }
+
+    private static int process(HashMap<String, Integer> dp, int[][] map, String target) {
+        if (dp.containsKey(target)) {
+            return dp.get(target);
+        }
+
+        char[] chars = target.toCharArray();
+        int[] tmap = new int[26];
+        //将目标字符串转换成词频的形式
+        for (char c : chars) {
+            tmap[c - 'a']++;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        //遍历每一张贴纸
+        for (int i = 0; i < map.length; i++) {
+            if (map[i][chars[0]-'a']==0){
+                continue;
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            //顺序遍历26个字母
+            for (int j = 0; j < 26; j++) {
+                //如果当前位置的字母数量>0,用当前的贴纸来处理这个字母
+                if (tmap[j] > 0) {
+                    for (int num = 0; num < Math.max(0, tmap[j]-map[i][j]); num++) {
+                        //将未处理的剩余字符拼接起来
+                        stringBuilder.append((char) ('a' + j));
+                    }
+                }
+            }
+            String t = stringBuilder.toString();
+            int result = process(dp, map, t);
+            if (result != -1) {
+                ans = Math.min(ans, 1 + result);
+            }
+        }
+        dp.put(target, ans == Integer.MAX_VALUE ? -1 : ans);
+        return dp.get(target);
+    }
+
 }
