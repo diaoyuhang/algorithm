52b4e1ced9b5219a6f527bc2321799cbbdce05cf
diaoyuhang <951900584@qq.com>
2022-03-03 19:52:12 +0800

    basicClass6

--- src/main/java/basicClass/class06/Code05_FindFirstIntersectNode.java
+++ src/main/java/basicClass/class06/Code05_FindFirstIntersectNode.java
@@ -1,4 +1,139 @@
 package basicClass.class06;
 
 public class Code05_FindFirstIntersectNode {
+
+    public static class Node {
+        public int value;
+        public Node next;
+
+        public Node(int data) {
+            this.value = data;
+        }
+    }
+
+
+    public static Node getIntersectNode(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+        Node loop1 = getLoopNode(head1);
+        Node loop2 = getLoopNode(head2);
+
+        if (loop1 == null && loop2 == null) {
+            return noLoop(head1, head2);
+        }
+
+        if (loop1 != null && loop2 != null) {
+            return bothLoop(head1, loop1, head2, loop2);
+        }
+        return null;
+    }
+
+    public static Node bothLoop(Node head1, Node loop1, Node head2, Node loop2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+
+        Node cur1 = head1;
+        Node cur2 = head2;
+        if (loop1 == loop2) {
+            int n = 0;
+
+            while (cur1 != loop1) {
+                n++;
+                cur1 = cur1.next;
+            }
+
+            while (cur2 != loop2) {
+                n--;
+                cur2 = cur2.next;
+            }
+            cur1 = n > 0 ? head1 : head2;
+            cur2 = cur1 == head1 ? head2 : head1;
+
+            n = Math.abs(n);
+
+            while (n > 0) {
+                cur1 = cur1.next;
+            }
+            while (cur1 != cur2) {
+                cur1 = cur1.next;
+                cur2 = cur2.next;
+            }
+            return cur1;
+        } else {
+            cur1 = loop1.next;
+            while (cur1 != loop1) {
+                if (cur1 == loop2) {
+                    return cur1;
+                }
+                cur1 = cur1.next;
+            }
+            return null;
+        }
+    }
+
+    public static Node noLoop(Node head1, Node head2) {
+        if (head1 == null || head2 == null) {
+            return null;
+        }
+
+        Node cur1 = head1;
+        Node cur2 = head2;
+        int n = 0;
+        while (cur1 != null) {
+            n++;
+            cur1 = cur1.next;
+        }
+
+        while (cur2 != null) {
+            cur2 = cur2.next;
+            n--;
+        }
+
+        cur1 = n > 0 ? head1 : head2;
+        cur2 = cur1 == head1 ? head2 : head1;
+
+        n = Math.abs(n);
+
+        while (n > 0) {
+            cur1 = cur1.next;
+            n--;
+        }
+
+        while (cur1 != cur2) {
+            if (cur1 == null || cur2 == null) {
+                return null;
+            }
+            cur1 = cur1.next;
+            cur2 = cur2.next;
+        }
+        return cur1;
+    }
+
+    //得到入环点
+    public static Node getLoopNode(Node head) {
+        if (head == null || head.next == null || head.next == null) {
+            return head;
+        }
+
+        Node slow = head.next;
+        Node fast = head.next.next;
+
+        while (slow != fast) {
+            if (fast.next == null || fast.next.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        fast = head;
+
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
 }
