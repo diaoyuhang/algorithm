39349bf772213c150e8e197b3d406aa698917541
YH-D <951900584@qq.com>
2022-03-28 21:44:02 +0800

    basicClass11

--- src/main/java/basicClass/class11/Code08_CardsInLine.java
+++ src/main/java/basicClass/class11/Code08_CardsInLine.java
@@ -13,8 +13,8 @@ public class Code08_CardsInLine {
         //先手
         int result1 = first(cards, 0, cards.length - 1);
         //后手
-        int result2 =second(cards,0,cards.length-1);
-        return Math.max(result1,result2);
+        int result2 = second(cards, 0, cards.length - 1);
+        return Math.max(result1, result2);
     }
 
     private static int first(int[] cards, int left, int right) {
@@ -35,9 +35,33 @@ public class Code08_CardsInLine {
         return Math.min(res1, res2);
     }
 
+    public static int dpWay(int[] cards) {
+        if (cards == null || cards.length == 0) {
+            return 0;
+        }
+
+        int[][] firstDp = new int[cards.length][cards.length];
+        int[][] secondDp = new int[cards.length][cards.length];
+
+        for (int i = 0; i < firstDp.length; i++) {
+            firstDp[i][i] = cards[i];
+            secondDp[i][i] = 0;
+        }
+
+        for (int right = 1; right < cards.length; right++) {
+            for (int left = right - 1; left >= 0; left--) {
+                firstDp[left][right] = Math.max(cards[left] + secondDp[left + 1][right], cards[right] + secondDp[left][right - 1]);
+                secondDp[left][right] = Math.min(firstDp[left + 1][right], firstDp[left][right - 1]);
+            }
+        }
+
+        return Math.max(firstDp[0][cards.length - 1], secondDp[0][cards.length - 1]);
+    }
+
     public static void main(String[] args) {
-        int[] arr = { 1, 9, 1,234,345,456,456,435,236,456,3521 };
+        int[] arr = {1, 9, 1, 234, 345, 456, 456, 435, 236, 456, 3521};
         System.out.println(cardsInLine(arr));
+        System.out.println(dpWay(arr));
 
     }
 }
