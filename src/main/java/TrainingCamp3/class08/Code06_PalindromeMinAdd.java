6f48bfe1c0b75267b2453060355ef93d224a5a54
YuHang-Diao <951900584@qq.com>
2022-09-06 22:47:10 +0800

    TrainingCamp3

--- src/main/java/TrainingCamp3/class08/Code06_PalindromeMinAdd.java
+++ src/main/java/TrainingCamp3/class08/Code06_PalindromeMinAdd.java
@@ -5,41 +5,4 @@ import java.util.HashMap;
 
 public class Code06_PalindromeMinAdd {
 
-
-    public static int getLen2(int[] arr) {
-        int res = 0;
-        int left = 0;
-        int right = 0;
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        int max = arr[0];
-        int min = arr[0];
-
-        while (right < arr.length) {
-
-            while (right < arr.length) {
-                if (hashMap.containsKey(arr[right])) {
-                    break;
-                } else {
-                    hashMap.put(arr[right], 1);
-                    max = Math.max(max, arr[right]);
-                    min = Math.min(min, arr[right]);
-
-                    if (max - min + 1 == hashMap.size()) {
-                        res = Math.max(res, max - min + 1);
-                    }
-                    right++;
-                }
-            }
-            hashMap.remove(arr[left]);
-            left++;
-        }
-        return res;
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {5, 5, 3, 2, 6, 4, 3};
-        System.out.println(getLen2(arr));
-
-    }
-
 }
